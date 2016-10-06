1cdef90f073097a45689ac7893bd942c4af5ec84
javierEh <javieresponda1995@gmail.com>
2016-10-06 17:59:22 -0500

    Prueba Javier

--- core/src/mx/itesm/Juego.java
+++ core/src/mx/itesm/Juego.java
@@ -3,7 +3,7 @@ package mx.itesm;
 import com.badlogic.gdx.Game;
 
 /**
- * Created by JoseCastilla on 01/09/2016.
+ * Created by JoseCastilla y Javier :) on 01/09/2016.
  */
 public class Juego extends Game{
 
