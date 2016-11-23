8550fbc4b39308948f0aba1ef3ffa22fd25ccb95
daniela <a01169706@itesm.mx>
2016-11-22 22:10:08 -0600

    Video al principio

--- core/src/mx/itesm/PantallaHistoria.java
+++ core/src/mx/itesm/PantallaHistoria.java
@@ -73,8 +73,6 @@ public class PantallaHistoria implements Screen {
 
     @Override
     public void render(float delta) {
-        //Gdx.gl.glClearColor(1,0,0,1);
-        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         cont++;
         escena.draw();
 
